93fe1723300d01d8635229b62ca34c42323bbf16
Manav <gangvanimanav@gmail.com>
2025-01-29 21:12:03 +0530

    codes

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // db.json is the file where your mock data will be
+const router = jsonServer.router("db.json"); // Path to your mock data or use a db file
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
+
 server.listen(3000, () => {
   console.log("JSON Server is running");
 });
